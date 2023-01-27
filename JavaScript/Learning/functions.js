5dc76c48fb1354865d8c11d45757f2d4f787df60
Jahirul Tusar <41430709+jahirultusar@users.noreply.github.com>
2023-01-27 15:41:59 +0000

    Create functions.js

---
+++ JavaScript/Learning/functions.js
@@ -0,0 +1,13 @@
+// Functions is a collection of statements that are used in the application to perform a specific task.  The functions are called from the main
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+var result1 = addNumbers(2, 3);
+var result2 = addNumbers(4, 5);
+
+console.log(result1);
+console.log(result2);
+
+
