f1cb57fec50ab30a07454c58af8f18848b360869
Jahirul Tusar <41430709+jahirultusar@users.noreply.github.com>
2023-01-27 15:19:10 +0000

    Create arrays.js

---
+++ JavaScript/Learning/arrays.js
@@ -0,0 +1,32 @@
+// Arrays are a special type of object that can hold multiple values
+
+// Arrays are in square brackets
+// Arrays are indexed starting from 0
+// Arrays are mutable
+// Arrays are ordered
+// Arrays are iterable
+// Arrays are objects
+
+var names = [
+    "Jahirul", 
+    "Tusar", 
+    "Rahman", 
+    "Rahim", 
+    "Karim"
+];
+
+var myArray = ["Jahirul", "Tusar", 25, false, {city: "London", country: "UK"}];
+
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+console.log(myArray);
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+console.log(myArray[4]);
+
