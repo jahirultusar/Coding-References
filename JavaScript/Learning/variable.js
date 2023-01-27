dd5a03a8c9505ba2ba6f65a2a10105dc270421b9
Jahirul Tusar <41430709+jahirultusar@users.noreply.github.com>
2023-01-27 14:17:56 +0000

    Create variable.js

---
+++ JavaScript/Learning/variable.js
@@ -0,0 +1,41 @@
+// What are variables?
+
+// variable is a container like a box. It can store any type of data. It can store a number, a string, a boolean, an array, an object, a function, etc.
+
+/* Rules for naming a variable:
+
+ *** variable name should be camleCase
+
+1. A variable name can contain letters, digits, underscores, and dollar signs.
+2. A variable name must begin with a letter
+3. A variable name can also begin with $ and _ (but we will not use it usually)
+4. Variable names are case sensitive (y and Y are different variables)
+5. Reserved words (like JavaScript keywords) cannot be used as variable names
+6. Variable names should be meaningful
+7. Variable names should not be too long
+8. Variable names should not be too short
+9. Variable names should not contain spaces
+10. Variable names should not contain special characters like !, @, #, $, %, ^, &, *, (, ), -, +, =, {, }, [, ], |, :, ;, <, >, ?, /, and so on.
+
+
+*/
+
+var firstName = "Jahirul";
+var lastName = "Tusar";
+var age = 25;
+var isMAle = true;
+var number = 100.50;
+var dob = new Date(1990, 11, 17);
+var person = {};
+var empty = null;
+var empty2 = undefined;
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof age);
+console.log(typeof isMAle);
+console.log(typeof number);
+console.log(typeof dob);
+console.log(typeof person);
+console.log(typeof empty);
+console.log(typeof empty2);
