b62176939b3ec1cecd02f8b8564d436c89fff1b4
Jahirul Tusar <41430709+jahirultusar@users.noreply.github.com>
2023-03-14 20:48:11 +0000

    forms.py

---
+++ flask/forms and validation/forms.py
@@ -0,0 +1,25 @@
+""" To create and validate forms we are gonna use WT Forms """
+# setup pip install Flask-WTF
+
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+
+class RegistrationForm(FlaskForm):
+    """Registration form for new users."""
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    """Login form for existing users."""
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
