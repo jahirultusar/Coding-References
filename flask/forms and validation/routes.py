f62a815780cf41d544bbf400e9ef1a20d7cf50d9
Jahirul Tusar <41430709+jahirultusar@users.noreply.github.com>
2023-03-15 15:07:19 +0000

    updated

--- flask/forms and validation/routes.py
+++ flask/forms and validation/routes.py
@@ -16,12 +16,25 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = RegistrationForm()
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'admin' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Wrong Username or Password', 'danger')
+            
     return render_template('login.html', title='Login', form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
+
+
+
